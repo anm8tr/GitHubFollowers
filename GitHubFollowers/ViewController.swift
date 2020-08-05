7be4af9544bc154dce92f12660d7a3e9d73db31f
anm8tr <threeduser@hotmail.com>
2020-08-04 18:05:55 -0700

    Initial Commit

---
+++ GitHubFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by Richard Garrison on 8/4/20.
+//  Copyright © 2020 Richard Garrison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
